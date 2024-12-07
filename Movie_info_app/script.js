47e3ad058a44cd03536901bdc033f04f15a8002f
Sumeet Gedam <sumeetgedam01@gmail.com>
2024-12-07 15:30:21 +0530

    Notes app and Image Search Engine

--- Movie_info_app/script.js
+++ Movie_info_app/script.js
@@ -1,12 +1,12 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const fetchFrom = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const fetchFromQuery = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search  = document.getElementById('search');
 
-getMovies(API_URL);
+getMovies(fetchFrom);
 
 async function getMovies(url) {
     const res = await fetch(url);
@@ -54,7 +54,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchTerm = search.value;
     if(searchTerm && searchTerm !== ''){
-        getMovies(SEARCH_API + searchTerm);
+        getMovies(fetchFromQuery + searchTerm);
         search.value = '';
     }else { 
         window.location.reload();
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # JS_Projects
 
+## Beginner level JavaScript projects
 
 ### [To-Do List App](https://sumeetgedam.github.io/JS_Projects/ToDo_list/)
 
@@ -9,13 +10,21 @@
 
 ![WeatherApp](./assets/weather_app.PNG)
 
+### [Image Search Engine](https://sumeetgedam.github.io/JS_Projects/Image_search_engine/)
+
+![IamgeSearchEngine](./assets/image_search_engine.PNG)
+### [QR Code Generator](https://sumeetgedam.github.io/JS_Projects/QR_code_generator/)
+
+![QRCodeGenerator](./assets/qr_code_generator.PNG)
+
+### [Notes App](https://sumeetgedam.github.io/JS_Projects/Notes_app/)
+
+![NotesApp](./assets/notes_app.PNG)
+
 ### [Movie Info app](https://sumeetgedam.github.io/JS_Projects/Movie_info_app/)
 
 ![MovieInfoApp](./assets/movie_info_landing_page.png)
 
-### [QR Code Generator](https://sumeetgedam.github.io/JS_Projects/QR_code_generator/)
-
-![QRCodeGenerator](./assets/qr_code_generator.PNG)
 
 ### [Mini Calendar](https://sumeetgedam.github.io/JS_Projects/Mini_calendar/)
 
